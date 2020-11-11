f6b192543a7060ba014613ca51c109e2d5f49180
ryanunruh <ryanunruhmedia@gmail.com>
2020-11-10 23:51:57 -0600

    Local storage established.

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 // Storage Controller
-const StorageCtrl = (function (){
-
-
+const StorageCtrl = (function () {
   return {
-    storeItem: function(item){
-      let items = []
-      if(localStorage.getItem('items')===null){
-
+    storeItem: function (item) {
+      let items;
+      if (localStorage.getItem("items") === null) {
+        items = [];
+        items.push(item);
+        localStorage.setItem("items", JSON.stringify(items));
       } else {
-
+        items = JSON.parse(localStorage.getItem("items"));
+        items.push(item);
+        localStorage.setItem("items", JSON.stringify(items));
       }
     },
+    getItemsFromStorage: function () {
+      let items;
+      if (localStorage.getItem("items") === null) {
+        items = [];
+      } else {
+        items = JSON.parse(localStorage.getItem("items"));
+      }
+      return items;
+    }
   };
-})();  
-
-
+})();
 
 // Item Controller
 const ItemCtrl = (function () {
@@ -27,11 +36,12 @@ const ItemCtrl = (function () {
 
   // Data Structure / State
   const data = {
-    items: [
-      // {id: 0, name: 'Steak Dinner', calories: 1200},
-      // {id: 1, name: 'Cookie', calories: 400},
-      // {id: 2, name: 'Eggs', calories: 300}
-    ],
+    // items: [
+    //   // {id: 0, name: 'Steak Dinner', calories: 1200},
+    //   // {id: 1, name: 'Cookie', calories: 400},
+    //   // {id: 2, name: 'Eggs', calories: 300}
+    // ],
+    items: StorageCtrl.getItemsFromStorage(),
     currentItem: null,
     totalCalories: 0,
   };
@@ -305,7 +315,7 @@ const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
       UICtrl.showTotalCalories(totalCalories);
 
       // Store in local storage
-      StorageCtrl.storeItem(newItem)
+      StorageCtrl.storeItem(newItem);
 
       // Clear fields
       UICtrl.clearInput();
@@ -409,7 +419,7 @@ const App = (function (ItemCtrl, StorageCtrl, UICtrl) {
       loadEventListeners();
     },
   };
-})(ItemCtrl,StorageCtrl UICtrl);
+})(ItemCtrl, StorageCtrl, UICtrl);
 
 // Initialize App
 App.init();
