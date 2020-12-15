4c7bddae33e6d77f2f3e47fe75004db0cf233433
ryanunruh <ryanunruhmedia@gmail.com>
2020-12-15 11:46:00 -0600

    Commented Code removed

--- app.js
+++ app.js
@@ -57,11 +57,6 @@ const ItemCtrl = (function () {
 
   // Data Structure / State
   const data = {
-    // items: [
-    //   // {id: 0, name: 'Steak Dinner', calories: 1200},
-    //   // {id: 1, name: 'Cookie', calories: 400},
-    //   // {id: 2, name: 'Eggs', calories: 300}
-    // ],
     items: StorageCtrl.getItemsFromStorage(),
     currentItem: null,
     totalCalories: 0,
